733bafe33b1ab418428883f82e2f18fd08278465
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-05 09:50:25 +0100

    Refactor stringLength function

--- stringLength.js
+++ stringLength.js
@@ -1,18 +1,6 @@
 const stringLength = (str) => {
-    if(!(str.length > 1)) 
-    {
-        throw new Error('MIN_LENGTH_ERROR: The string should be at least 1 character long');
-    }
-    else
-    {
-        if(!(str.length <= 10)) 
-        {
-            throw new Error('MAX_LENGTH_ERROR: The string should not be longer than 10 characters.');
-        }
-        else {
-            return str.length;
-        }
-    }
-    
+    if(!(str.length > 1)) throw new Error('MIN_LENGTH_ERROR: The string should be at least 1 character long');
+    if(!(str.length <= 10)) throw new Error('MAX_LENGTH_ERROR: The string should not be longer than 10 characters.');
+    return str.length;
 };
 module.exports = stringLength;
