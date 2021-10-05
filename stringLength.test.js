156e154e33812eb42a8c1dc1b26dd514e883a6f0
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-05 09:48:41 +0100

    Test for exceptions thrown if the length is wrong

--- stringLength.test.js
+++ stringLength.test.js
@@ -5,24 +5,25 @@ test('returns the correct length of simple words', () => {
     expect(stringLength('banana')).toBe(6);
     expect(stringLength('subpoena')).toBe(8);
     expect(stringLength('archeology')).toBe(10);
-    expect(stringLength('pneumonoultramicroscopicsilicovolcanoconiosis')).toBe(45);
 });
 
-test('works with empty string', () => {
-    expect(stringLength('')).toBe(0);
-    expect(stringLength(new String())).toBe(0);
-    expect(stringLength("")).toBe(0);
-    expect(stringLength(``)).toBe(0);
+test('throws exception on empty string', () => {
+    expect(() => {stringLength('')}).toThrow(/MIN_LENGTH_ERROR/);
+    expect(() => {stringLength(new String())}).toThrow(/MIN_LENGTH_ERROR/);
+    expect(() => {stringLength("")}).toThrow(/MIN_LENGTH_ERROR/);
+    expect(() => {stringLength(``)}).toThrow(/MIN_LENGTH_ERROR/);
 });
 
 test('counts special characters as well', () => {
-    expect(stringLength('zerumbet-platrier')).toBe(17);
-    expect(stringLength("   !sdopn&osà")).toBe(13);
-    expect(stringLength("--è_opajaoqoe&oéàldsq?")).toBe(22);
+    expect(() => {stringLength('zerumbet-platrier')}).toThrow(/MAX_LENGTH_ERROR/);
+    expect(stringLength("!sdopn&osà")).toBe(10);
+    expect(stringLength("éàldsq?")).toBe(7);
 });
 
 test('is not case sensitive', () => {
-    expect(stringLength('femme battante')).toBe(14);
-    expect(stringLength("yAtOUT. Il faut COMMandER.")).toBe(26);
-    expect(stringLength("WORKS WITH ALL CAPS")).toBe(19);
+    expect(stringLength('femmE')).toBe(5);
+    expect(() => {stringLength("yAtOUT. Il faut COMMandER.")}).toThrow(/MAX_LENGTH_ERROR/);
+    expect(stringLength("camelCase")).toBe(9);
+    expect(stringLength("cApiTaL")).toBe(7);
+    expect(stringLength("ALLCAPS")).toBe(7);
 });
