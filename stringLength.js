df10b63bada11455c342c1d103c874f830b6646c
King Josaphat Chewa <26507383+Kingjosh007@users.noreply.github.com>
2021-10-05 09:32:55 +0100

    Update stringLength function to throw exceptions

--- stringLength.js
+++ stringLength.js
@@ -1,2 +1,6 @@
-const stringLength = (str) => str.length;
+const stringLength = (str) => {
+    if(!(str.length > 1)) throw new Error('MIN_LENGTH_ERROR: The string should be at least 1 character long');
+    if(!(str.length <= 10)) throw new Error('MAX_LENGTH_ERROR: The string should not be longer than 10 characters.');
+    return str.length;
+};
 module.exports = stringLength;
